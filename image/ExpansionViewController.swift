070741b72544e9456211c9a5187e75e09031b71e
makoto.ono <r100rs900ss@icloud.com>
2021-02-23 18:06:17 +0900

    first

---
+++ image/ExpansionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ExpansionViewController.swift
+//  image
+//
+//  Created by 小野真 on 2021/02/23.
+//
+
+import UIKit
+
+class ExpansionViewController: UIViewController {
+    //戻るをoutletしておく
+    @IBOutlet weak var back: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    Var Img: UIImage!
+    //受け取るための変数を宣言しておく
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- image/ViewController.swift
+++ image/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tapaction: UITapGestureRecognizer!
+    @IBOutlet weak var slide: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        
+       
+        //バンドルした画像ファイルを読み込み
+        let Image = UIImage(named: "german.jpg")
         // Do any additional setup after loading the view.
+        slide.image = Image
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        //segueから遷移先のExpansionviewControllerを取得する
+        let expansionViewController:ExpansionViewController = segue.destination as! ExpansionViewController
+        
+        expansionViewController.image = Image
+    }
+    
+   @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+   }
+    
 }
 
